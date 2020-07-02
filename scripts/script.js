ec766f52d91216f1af48ae3f64243ba6703c35c4
Your Name <svedlana@yandex.ru>
2020-07-02 22:13:42 +0300

    Fix: исправить замечания первого код-ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup'); /* в код-ревью написано "Элемент нигде не используется далее. Необходимо удалить", 
+я отметила "исправлено", но удалять не стала, так как видимо не было замечено, что этот элемент используется в фунцкии popupToggle
+ - первая функция под константами*/
 const popupProfile = document.querySelector('.popup_type_profile');
 const buttonOpenProfile = document.querySelector('.profile__edit');
 const popupSaveProfile = popupProfile.querySelector('.popup__form');
@@ -12,18 +14,17 @@ const buttonAddElement = document.querySelector('.profile__add');
 const popupSaveElement = popupElement.querySelector('.popup__form_element');
 const cardsList = document.querySelector('.elements__items');
 const cardsItem = cardsList.querySelector('.elements__item');
-const cardsTemplate = document.querySelector('.elements__template');
+const cardsTemplate = document.querySelector('.elements__template').content;
 const inputTitle = popupSaveElement.querySelector('.popup__input_subject_pic-title');
 const inputLink = popupSaveElement.querySelector('.popup__input_subject_pic-link');
-const formElement = popup.querySelector('.popup__form');
 
-const loyout = document.querySelector('.popup_type_loyout');
-const loyoutTitle = loyout.querySelector('.popup__title_loyout');
-const loyoutImg = loyout.querySelector('.popup__img');
+const layout = document.querySelector('.popup_type_layout');
+const layoutTitle = layout.querySelector('.popup__title_layout');
+const layoutImg = layout.querySelector('.popup__img');
 
 const buttonCloseProfile = popupProfile.querySelector('.popup__close');
 const buttonCloseElement = popupElement.querySelector('.popup__close');
-const buttonCloseLoyout = loyout.querySelector('.popup__close');
+const buttonCloselayout = layout.querySelector('.popup__close');
 
 function popupToggle(popup) {
     popup.classList.toggle('popup_opened');
@@ -36,30 +37,29 @@ function profileFormSubmit(evt) {
     popupToggle(popupProfile);
 }
 
-buttonOpenProfile.addEventListener('click', function () {
-    popupToggle(popupProfile);
-    if (popupProfile.classList.contains('popup_opened')) {
-        inputName.value = profileName.textContent;
-        inputJob.value = profileJob.textContent;
-    }
-})
+    buttonOpenProfile.addEventListener('click', function () {
+        popupToggle(popupProfile);    
+            inputName.value = profileName.textContent;
+            inputJob.value = profileJob.textContent;
+        
+    })
 
-popupSaveProfile.addEventListener('submit', profileFormSubmit);
+    popupSaveProfile.addEventListener('submit', profileFormSubmit);
 
-buttonCloseProfile.addEventListener('click', function () {
-    popupToggle(popupProfile);
-})
+    buttonCloseProfile.addEventListener('click', function () {
+        popupToggle(popupProfile);
+    })
 
-cards.forEach(function (item) {
-    elementAdd(item);
-});
+    cards.forEach(function (item) {
+        elementAdd(item);
+    });
 
-function elementAdd(item) {
-    const cardsTemplate = document.querySelector('.elements__template').content;
+function elementCreate(item) {
     const card = cardsTemplate.cloneNode(true);
     card.querySelector('.elements__title').textContent = item.name;
     card.querySelector('.elements__img').src = item.link;
     card.querySelector('.elements__img').alt = item.name;
+
     card.querySelector('.elements__like').addEventListener('click', function (evt) {
         evt.target.classList.toggle('elements__like_active');
     });
@@ -67,16 +67,19 @@ function elementAdd(item) {
         const element = evt.target.closest('.elements__item');
         element.remove();
     });
-
     card.querySelector('.elements__img').addEventListener('click', function() {
-        popupToggle(loyout);
-        loyoutTitle.textContent  = item.name;
-        loyoutImg.src = item.link;
-      });
+        popupToggle(layout);
+        layoutTitle.textContent  = item.name;
+        layoutImg.src = item.link;
+      }); 
+    return card;    
+      }
 
-    cardsList.prepend(card);  
-}
-
+function elementAdd(item) {
+    let element = elementCreate(item);
+    cardsList.prepend(element);
+        }
+    
 function elementFormSubmit(evt) {
     evt.preventDefault();
     let name = inputTitle.value;
@@ -85,20 +88,18 @@ function elementFormSubmit(evt) {
     popupToggle(popupElement);
 }
 
-buttonAddElement.addEventListener('click', function () {
-    popupToggle(popupElement); 
-        if (popupElement.classList.contains('popup_opened')) {
+    buttonAddElement.addEventListener('click', function () {
+        popupToggle(popupElement); 
             inputTitle.value = '';
             inputLink.value = '';
-        }        
+        })
+        
+    popupSaveElement.addEventListener('submit',elementFormSubmit);
+
+    buttonCloseElement.addEventListener('click', function () {
+        popupToggle(popupElement);  
     })
-    
-popupSaveElement.addEventListener('submit',elementFormSubmit);
 
-buttonCloseElement.addEventListener('click', function () {
-    popupToggle(popupElement);  
-})
-
-buttonCloseLoyout.addEventListener('click', function () {
-    popupToggle(loyout);
-})
+    buttonCloselayout.addEventListener('click', function () {
+        popupToggle(layout);
+    })
